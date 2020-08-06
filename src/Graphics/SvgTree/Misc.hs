88750d6d642f7b494d9707c3796e5283ea7398f1
David Himmelstrup <lemmih@gmail.com>
2020-08-06 13:38:56 +0800

    Support the Asterius haskell compiler.

--- src/Graphics/SvgTree/Misc.hs
+++ src/Graphics/SvgTree/Misc.hs
@@ -1,16 +1,25 @@
+{-# LANGUAGE CPP #-}
 module Graphics.SvgTree.Misc
   ( ppD
   , ppF
   ) where
 
+#ifdef ASTERIUS
+import           Numeric
+#else
 import           Data.Double.Conversion.Text
 import qualified Data.Text                   as T
+#endif
 
 precision :: Int
 precision = 6
 
 ppD :: Double -> String
+#ifdef ASTERIUS
+ppD v = showFFloat (Just precision) v ""
+#else
 ppD = T.unpack . T.dropWhileEnd (== '.') . T.dropWhileEnd (== '0') . toFixed precision
+#endif
 
 ppF :: Float -> String
 ppF = ppD . realToFrac
