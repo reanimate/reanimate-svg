92fed67f03313421c00ac67ade024b7249d8f18c
David Himmelstrup <lemmih@gmail.com>
2020-09-26 13:13:41 +0800

    Get rid of unused executable.

--- executable/Main.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Graphics.SvgTree ( loadSvgFile )
-import System.Environment ( getArgs )
-import Text.Show.Pretty ( pPrint )
-
-main :: IO ()
-main = do
-  (f:_) <- getArgs
-  loadSvgFile f >>= pPrint
